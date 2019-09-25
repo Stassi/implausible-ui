b17641a9dd5c73f95b212c85b059e4248bc700f7
Andreas Stassivik <andreas@stassi.net>
2019-09-24 22:51:22 -0700

    parse *.md files

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader'
+    })
+
+    return config
+  }
+}
+
+module.exports = nextConfig
